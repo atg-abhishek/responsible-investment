933a1eecefdf127dcc9949797e572904d743d4d7
atg.abhishek <atg.abhishek@gmail.com>
2017-07-14 23:25:43 -0400

    initial server config

---
+++ backend/server.py
@@ -0,0 +1,16 @@
+from flask import Flask 
+import sys 
+
+app = Flask(__name__)
+
+HOST=None
+
+if len(sys.argv)>1 and sys.argv[1] == "prod":
+    HOST = '0.0.0.0'
+
+@app.route('/')
+def hello():
+    return 'hello'
+
+if __name__ == "__main__":
+    app.run(debug=True, host=HOST)
