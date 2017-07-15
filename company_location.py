ce1e77a719f9bc076085d5eeae163d3dbba377ee
sneha <sneha.desai@mail.mcgill.ca>
2017-07-15 12:50:11 -0400

    company locations added

--- company_location.py
+++ company_location.py
@@ -1,13 +1,18 @@
-from googleplaces import GooglePlaces, types, lang
+import urllib.parse, json, urllib.request, urllib
+def company_locations(company_name):
+    locations = []
+    # compnay_name = urllib.quote(company_name)
+    # print(company_name)
+    params = {'key': 'AIzaSyAfi34BQ1OEkIn0W-kzDl__Php_aVxnb38', 'query': company_name}
+    url = "https://maps.googleapis.com/maps/api/place/textsearch/json?{}".format(urllib.parse.urlencode(params))
+    with urllib.request.urlopen(url) as url:
+        data = json.loads(url.read().decode())
+        for i in range(len(data['results'])):
+            location = data["results"][i]["formatted_address"]
+            locations.append(location)
+    return locations
 
-YOUR_API_KEY = 'AIzaSyAfi34BQ1OEkIn0W-kzDl__Php_aVxnb38'
 
-google_places = GooglePlaces(YOUR_API_KEY)
 
-# import pudb; pu.db
-
-query_result = google_places.nearby_search(
-        location='London, England', keyword='Fish and Chips',
-        radius=20000, types=[types.TYPE_FOOD])
 
 
